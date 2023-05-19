7910b811c32505433cc76b5ac90a0f10cd4b8956
Nhykhe <Nhykhe@gmail.com>
2023-05-18 21:17:41 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main-This is a program that include printf
+ * Return 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,"\n);
+return (o);
+}
