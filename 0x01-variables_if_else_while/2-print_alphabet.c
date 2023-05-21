9c13d630836facc279cb293cf9b3bfc4f666c4a7
Nhykhe <nhykhe@gmail.com>
2023-05-21 11:42:18 -0700

    1-last_digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/** main - prints the alphabet
+ * Return: Always (Success)
+ */
+int main(void)
+#include <stdio.h>
+	/** main - prints the alphabet
+	 * Return: Always (Success)
+	 */
+	int main(void)
+#include <stdio.h>
+	/** main - prints the alphabet
+	 * Return: Always (Success)
+	 */
+	int main(void)
+
