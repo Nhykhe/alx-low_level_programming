80e7bd4574c8e1673ed4ed95499beda97449f59b
Nhykhe <nhykhe@gmail.com>
2023-05-22 09:43:17 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print number from 0 to 10
+ * Return:Always 0;
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+return (0);
+}
