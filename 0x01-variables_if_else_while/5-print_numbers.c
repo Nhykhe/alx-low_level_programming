894598fef56699ef533ede3e4d7554a30d4bd37a
Nhykhe <nhykhe@gmail.com>
2023-05-21 23:19:20 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all digit number of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		putchar(num);
+	}
+return (0);
+}
