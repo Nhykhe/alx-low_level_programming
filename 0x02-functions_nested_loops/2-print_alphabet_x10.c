e9aed1cdbad216572d6505994bfe65c4fcb3e80c
Nhykhe <nhykhe@gmail.com>
2023-05-27 14:33:20 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphbet_x10 - prints the alphabet x10
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	char c = 'a';
+	int count = 0;
+
+	while (count < 10 * 26)
+	{
+		_putchar(c);
+		count++;
+		if (count % 26 == 0)
+		{
+			_putchar('\n');
+			c = 'a';
+		}
+		else
+		{
+			c++;
+		}
+	}
+}
