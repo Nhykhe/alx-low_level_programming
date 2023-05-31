47969d6fa1028aab22e5a6b069772e944575f3a7
Nhykhe <nhykhe@gmail.com>
2023-05-31 13:23:03 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main-This is a program that include printf
+ * main - A program that prints with printf
  * Return 0 (success)
  */
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n);
-return (o);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
