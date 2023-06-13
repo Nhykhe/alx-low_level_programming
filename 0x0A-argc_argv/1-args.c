d0772a5a066d17ca14a0aebb452197c484ea6a2a
Nhykhe <nhykhe@gmail.com>
2023-06-13 14:24:04 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the number
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("Number of arguments: %d\n", argc);
+		return (0);
+}
